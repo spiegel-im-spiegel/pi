de070f0f443d90d5ad712b49ef3a6d17ff5108ab
Spiegel <spiegel.im.spiegel@gmail.com>
2016-11-09 18:19:30 +0900

    add logic for histogram.

--- cmd/estmt.go
+++ cmd/estmt.go
@@ -8,13 +8,19 @@ import (
 	"github.com/spiegel-im-spiegel/pi/estmt"
 )
 
+// Flags
+var (
+	histClass float64
+	aaa       float64
+)
+
 // estmtCmd represents the estmt command
 var estmtCmd = &cobra.Command{
 	Use:   "estmt",
 	Short: "Estimate of Pi",
 	Long:  "Estimate of Pi with Monte Carlo method.",
 	Run: func(cmd *cobra.Command, args []string) {
-		if err := estmt.Execute(estmt.NewContext(cmd.OutOrStdout(), os.Stderr, pointCount, estmtCount)); err != nil {
+		if err := estmt.Execute(estmt.NewContext(cmd.OutOrStdout(), os.Stderr, pointCount, estmtCount, histClass)); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			ExitCode = 1
 		}
@@ -25,4 +31,5 @@ func init() {
 	RootCmd.AddCommand(estmtCmd)
 	estmtCmd.PersistentFlags().Int64VarP(&pointCount, "pcount", "p", 10000, "Count of points")
 	estmtCmd.PersistentFlags().Int64VarP(&estmtCount, "ecount", "e", 100, "Count of estimate")
+	estmtCmd.PersistentFlags().Float64VarP(&histClass, "hclass", "c", 0.0, "Class interval of histogram")
 }
--- estmt/estmt.go
+++ estmt/estmt.go
@@ -3,6 +3,7 @@ package estmt
 import (
 	"fmt"
 	"io"
+	"math"
 
 	"github.com/spiegel-im-spiegel/gocli"
 	"github.com/spiegel-im-spiegel/pi/genpi"
@@ -13,11 +14,12 @@ type Context struct {
 	ui            *gocli.UI
 	pointCount    int64
 	estimateCount int64
+	histClass     float64
 }
 
 //NewContext returns Context instance
-func NewContext(w, e io.Writer, pc, ec int64) *Context {
-	return &Context{ui: gocli.NewUI(nil, w, e), pointCount: pc, estimateCount: ec}
+func NewContext(w, e io.Writer, pc, ec int64, hc float64) *Context {
+	return &Context{ui: gocli.NewUI(nil, w, e), pointCount: pc, estimateCount: ec, histClass: hc}
 }
 
 //Execute returns list of points.
@@ -30,8 +32,53 @@ func Execute(cxt *Context) error {
 	}
 
 	ch := genpi.New(cxt.pointCount, cxt.estimateCount)
+	min := float64(10)
+	max := float64(0)
+	sum := float64(0)
+	sum2 := float64(0)
+	pis := make([]float64, 0, cxt.estimateCount)
 	for pi := range ch {
-		cxt.ui.Outputln(fmt.Sprintf("%v", pi))
+		pis = append(pis, pi)
+		if pi < min {
+			min = pi
+		}
+		if pi > max {
+			max = pi
+		}
+		sum += pi
+		sum2 += pi * pi
+	}
+	cxt.ui.OutputErrln(fmt.Sprintf("min: %v", min))
+	cxt.ui.OutputErrln(fmt.Sprintf("max: %v", max))
+	ave := sum / float64(cxt.estimateCount)
+	cxt.ui.OutputErrln(fmt.Sprintf("ave: %v", ave))
+	vari := sum2/float64(cxt.estimateCount) - ave*ave
+	cxt.ui.OutputErrln(fmt.Sprintf("dev: %v", math.Sqrt(vari)))
+
+	if cxt.histClass <= 0.0 {
+		for _, pi := range pis {
+			cxt.ui.Outputln(fmt.Sprintf("%v", pi))
+		}
+		return nil
+	}
+
+	classCount := int((max-min)/cxt.histClass) + 1
+	freqs := make([]int, classCount)
+	cxt.ui.OutputErrln(fmt.Sprintf("num: %v", classCount))
+	for _, pi := range pis {
+		class := 0
+		for i := 0; i < classCount; i++ {
+			if min+cxt.histClass*float64(i) <= pi {
+				class = i
+			} else {
+				break
+			}
+		}
+		freqs[class]++
+	}
+	for i, freq := range freqs {
+		mid := min + cxt.histClass*float64(i) + cxt.histClass/2.0
+		cxt.ui.Outputln(fmt.Sprintf("%v\t%v", mid, freq))
 	}
 
 	return nil
