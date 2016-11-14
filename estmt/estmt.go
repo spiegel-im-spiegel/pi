ad110d5fca01c3742e74e8d48f8d3a29c78a7fce
Spiegel <spiegel.im.spiegel@gmail.com>
2016-11-14 15:22:01 +0900

    statistics

--- estmt/estmt.go
+++ estmt/estmt.go
@@ -3,6 +3,7 @@ package estmt
 import (
 	"fmt"
 	"io"
+	"math"
 
 	"github.com/spiegel-im-spiegel/gocli"
 	"github.com/spiegel-im-spiegel/pi/genpi"
@@ -29,10 +30,39 @@ func Execute(cxt *Context) error {
 		return fmt.Errorf("invalid argument \"%v\" for ecount option", cxt.estimateCount)
 	}
 
+	//measurement
 	ch := genpi.New(cxt.pointCount, cxt.estimateCount)
+	min := float64(10)
+	max := float64(0)
+	sum := float64(0)
+	sum2 := float64(0)
+	pis := make([]float64, 0, cxt.estimateCount)
 	for pi := range ch {
 		cxt.ui.Outputln(fmt.Sprintf("%v", pi))
+		pis = append(pis, pi)
+		if pi < min {
+			min = pi
+		}
+		if pi > max {
+			max = pi
+		}
+		sum += pi
+		sum2 += pi * pi
 	}
+
+	//statistics
+	cxt.ui.OutputErrln(fmt.Sprintf("minimum value: %7.5f", min))
+	cxt.ui.OutputErrln(fmt.Sprintf("maximum value: %7.5f", max))
+	ave := sum / float64(cxt.estimateCount)
+	cxt.ui.OutputErrln(fmt.Sprintf("average value: %7.5f", ave))
+	devi := math.Sqrt(sum2/float64(cxt.estimateCount) - ave*ave) //variance -> standard deviation
+	ct := int64(0)
+	for _, pi := range pis {
+		if ave-devi <= pi && pi <= ave+devi {
+			ct++
+		}
+	}
+	cxt.ui.OutputErrln(fmt.Sprintf("standard deviation: %7.5f (%4.1f%%)", devi, float64(ct)*100.0/float64(cxt.estimateCount)))
 
 	return nil
 }
